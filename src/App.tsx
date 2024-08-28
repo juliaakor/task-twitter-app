b8be6f625bbad96bf13a81d91ad44992f3757594
Julia K <korchkova.yulia@gmail.com>
2024-08-28 22:23:19 +0300

    feat: add styled-components n basic global styles
      - Added styled-components package for styles
      - Added basic global styles for test (will be updated when theme will
    be added)
      - Added ThemeProvider and GlobalStyle to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './global';
+
 export const App = () => {
-  return (<></>)
+  return (
+    <ThemeProvider theme={{}}>
+      <GlobalStyle />
+      <>App</>
+    </ThemeProvider>
+  )
 }
