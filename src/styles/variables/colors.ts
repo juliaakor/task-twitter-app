31e5b7d0ce0950fbad5d4fd0395fa9ceab91817c
Julia K <korchkova.yulia@gmail.com>
2024-08-30 15:13:24 +0300

    style: add style variables and new aliases paths
      - Added styles path to app and common tsconfig file
      - Added the following variables: spacing, size, radius, fontFamily,
    fontSize, fontWeight, lineHeight, darkColors, lightColors
      - Added index file for variables export

---
+++ src/styles/variables/colors.ts
@@ -0,0 +1,23 @@
+export const lightColors = {
+  bgButtonBlack: 'rgba(0, 0, 0, 1)',
+  bgButtonDisabled: 'rgba(179, 184, 187, 1)',
+  bgPopup: 'rgba(247, 249, 249, 1)',
+  bgPrimary: 'rgba(255, 255, 255, 1)',
+  borderButton: 'rgba(0, 0, 0, 0.4)',
+  borderGrayButton: 'rgba(228, 234, 237, 1)',
+  borderInput: 'rgba(0, 0, 0, 0.2)',
+  hightlightBrand: 'rgba(29, 161, 242, 1)',
+  hightlightCopyright: 'rgba(83, 100, 113, 1)',
+  hightlightLike: 'rgba(239, 28, 92, 1)',
+  textButton: 'rgba(255, 255, 255, 1)',
+  textInfo: 'rgba(130, 130, 130, 1)',
+  textLink: 'rgba(29, 161, 242, 1)',
+  textPlaceholder: 'rgba(0, 0, 0, 0.6)',
+  textPrimary: 'rgba(0, 0, 0, 1)',
+  transparent: 'transparent',
+};
+
+export const darkColors = {
+  ...lightColors,
+  bgPrimary: 'rgba(0, 0, 0, 1)',
+};
