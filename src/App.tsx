abd4308816686575c31adc6f100171dfcba21902
Julia K <korchkova.yulia@gmail.com>
2024-08-30 16:25:40 +0300

    feat: add routes and navigation constants
      - Added react-router-dom package for routing
      - Added the following relative paths for easier access to modules:
    constants(constants required for the app)
      - Added ROUTES constants to use variable routes consistently throughout
    the app and added NAV_ROUTES for side bar options
      - Added a router file with info about routes, what path the routes use
    and components
      - Added import file for convenient importing of pages instead of
    importing one by one
      - Updated App component to include router and configured BrowserRouter
    and all routes available in the future

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { getTheme, GlobalStyles } from '@styles/index';
+
+import { router } from './router';
 
 export const App = () => {
   return (
     <ThemeProvider theme={getTheme()}>
       <GlobalStyles />
-      <div>App</div>
+      <BrowserRouter>
+        <Routes>
+          {router.map(({ Component, path }) => {
+            return <Route Component={Component} key={path} path={path} />;
+          })}
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
