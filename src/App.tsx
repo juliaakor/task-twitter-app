aa87f085bf9eafb5cedb58558c20c0aaa3843090
Julia K <korchkova.yulia@gmail.com>
2024-08-30 20:37:31 +0300

    feat: update ThemeContext to use theme from redux
      - Updated ThemeContext to depend on theme from the redux to save the
    theme state
      - Moved theme and store providers from App component to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-
-import { getTheme, GlobalStyles } from '@styles/index';
 
 import { router } from './router';
 
 export const App = () => {
   return (
-    <ThemeProvider theme={getTheme()}>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          {router.map(({ Component, path }) => {
-            return <Route Component={Component} key={path} path={path} />;
-          })}
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        {router.map(({ Component, path }) => (
+          <Route Component={Component} key={path} path={path} />
+        ))}
+      </Routes>
+    </BrowserRouter>
   );
 };
--- src/components/ThemeContext/types.ts
+++ src/components/ThemeContext/types.ts
@@ -1,13 +1,3 @@
-export enum Theme {
-  Light = 'light',
-  Dark = 'dark',
-}
-
 export interface ThemeProviderProps {
   children?: React.ReactNode;
-}
-
-export interface ContextProps {
-  darkTheme: boolean;
-  toggleTheme: () => void;
 }
