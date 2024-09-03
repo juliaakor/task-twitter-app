99b4e76745ad9b37345c2bd30ef216b99157f892
Julia K <korchkova.yulia@gmail.com>
2024-09-03 07:15:53 +0300

    fix: add module type and change cypress to js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3007',
-    setupNodeEvents(on, config) {},
   },
 });
