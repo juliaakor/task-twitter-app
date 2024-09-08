5248e047bef85a1c0328d14a30f95b9b2f829a2c
Julia K <korchkova.yulia@gmail.com>
2024-09-08 13:35:14 +0300

    feat: add Dropdown component and types for options
    Added styled Dropdown component that includes DropdownOption and styles,
    along with types

---
+++ src/components/Dropdown/styled.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const DropdownContainer = styled.div`
+  position: absolute;
+  max-height: 30vh;
+  overflow-y: scroll;
+
+  ${({ theme }) => `
+    background: ${theme.colors.bgPopup};
+    border-radius: ${theme.radius.small};
+    box-shadow: ${theme.spacing.none}px ${theme.spacing.none}px ${theme.size.small3X}px ${theme.spacing.none}px ${theme.colors.borderGrayButton};
+    padding: ${theme.spacing.small3X} ${theme.spacing.none};
+  `}
+`;
+
+export const DropdownOptions = styled.ul`
+  list-style-type: none;
+`;
+
+export const DropdownOption = styled.li`
+  ${({ theme }) => `
+    padding: ${theme.spacing.small3X} ${theme.spacing.mediumX};
+    color: ${theme.colors.textPrimary};
+
+
+    &:hover {
+      background: ${theme.colors.bgButtonHover};
+    }
+  `}
+`;
