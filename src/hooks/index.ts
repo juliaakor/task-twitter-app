e4f77bdccf9346610086e6f5b5d0a1bd9b2805bf
Julia K <korchkova.yulia@gmail.com>
2024-09-12 21:12:55 +0300

    feat: add useStorageUpload hook to save images
      - Updated exports for hooks folder
      - Added useStorageUpload to store tweets images in the store in tweets
    folder

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { useAuth } from './useAuth';
 export { useModal } from './useModal';
 export { useToggleMenu } from './useToggleMenu';
+export { useStorageUpload } from './useStorageUpload';
 export { useError } from './useError';
