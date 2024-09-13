3452d786bdd697b97edc29f3f79925723bebe137
Julia K <korchkova.yulia@gmail.com>
2024-09-13 05:46:56 +0300

    feat: add luxon and getTimeAgoStringFromDate util

---
+++ src/lib/format/getTimeAgoStringFromDate.ts
@@ -0,0 +1,7 @@
+import { DateTime } from 'luxon';
+
+export const getTimeAgoStringFromDate = (createdDate: string) => {
+  const dateTime = DateTime.fromISO(createdDate);
+
+  return dateTime.toRelative();
+};
