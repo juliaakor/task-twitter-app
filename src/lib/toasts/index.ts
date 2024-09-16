a1080f7162ff9391777e342eda2d459b60bc591f
Julia K <korchkova.yulia@gmail.com>
2024-09-16 04:59:34 +0300

    feat: add password update toasts

--- src/lib/toasts/index.ts
+++ src/lib/toasts/index.ts
@@ -2,25 +2,34 @@ import toast, { DefaultToastOptions } from 'react-hot-toast';
 
 import { getTheme } from '@styles/theme';
 
+const { colors } = getTheme();
+
 const baseToastConfig: DefaultToastOptions = {
   ariaProps: {
     'aria-live': 'polite',
     role: 'status',
   },
   position: 'top-right',
-  style: {},
-};
-
-const { colors } = getTheme();
-
-const failToastConfig = {
-  ...baseToastConfig,
-  duration: 6000,
-  icon: '💡',
   style: {
     backgroundColor: colors.hightlightBrand,
     color: colors.textButton,
   },
 };
 
+const successToastConfig = {
+  ...baseToastConfig,
+  duration: 4000,
+  icon: '👏',
+};
+
+const failToastConfig = {
+  ...baseToastConfig,
+  duration: 6000,
+  icon: '💡',
+};
+
 export const failLoadMoreImagedToast = () => toast('Sorry, you cannot add more than 4 images', failToastConfig);
+
+export const passwordResetSuccess = () => toast('Password was updated', successToastConfig);
+
+export const passwordResetFail = () => toast('Sorry, couldnt update the password, try again later', failToastConfig);
