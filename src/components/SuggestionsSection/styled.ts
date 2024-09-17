101f8d96dea41517b1ce6f900c7acc535c13a320
Julia K <korchkova.yulia@gmail.com>
2024-09-17 08:45:40 +0300

    feat: add SuggestionsSection component

---
+++ src/components/SuggestionsSection/styled.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const SuggestionsContainer = styled.div`
+  ${({ theme }) => `
+    background: ${theme.colors.bgPopup};
+    margin: ${theme.spacing.medium} ${theme.spacing.none};
+    border-radius: ${theme.radius.medium};
+    padding: ${theme.spacing.medium};
+  `}
+`;
+
+export const SuggestionsHeader = styled.div`
+  ${({ theme }) => `
+    font-weight: ${theme.fontWeight.bold};
+    margin-bottom: ${theme.spacing.medium};
+    font-family: ${theme.fontFamily.secondary};
+    font-size: ${theme.fontSize.large3X};
+    line-height: ${theme.lineHeight.large};
+  `}
+`;
+
+export const UsersContainer = styled.div`
+  max-height: 50vh;
+  overflow-y: auto;
+`;
